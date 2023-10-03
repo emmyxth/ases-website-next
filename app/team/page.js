554895818435ad50309c5add0549ef48c75a421a
emmyxth <emmyst@stanford.edu>
2023-10-02 21:13:32 -0700

    done with team page - fixed bug of space at bottom

--- app/team/page.js
+++ app/team/page.js
@@ -1,7 +1,7 @@
 import Apply from "@/components/team/Apply.jsx";
 import Footer from "@/components/general/Footer";
 import Header from "@/components/team/Header.jsx";
-import TeamProfiles from "@/components/team/TeamProfiles.jsx";
+import TeamProfiles from "@/components/team/TeamProfiles.js";
 import TopBar from "@/components/general/Topbar";
 import Box from "@mui/material/Box";
 
--- components/team/TeamProfiles.js
+++ components/team/TeamProfiles.js
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 import Box from "@mui/material/Box";
 import React, { useState } from "react";
-import styles from "./TeamProfiles.module.css"
+import styles from "./TeamProfiles.module.css";
 import ProfileCard from "../general/ProfileCard";
 import profileData from "@/public/textContent/teamProfileCards.json";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
@@ -9,50 +9,55 @@ import IconButton from "@mui/material/IconButton";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 
 export default function TeamProfile() {
-    const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
-    const handleNextCard = () => {
-      setCurrentCardIndex((prevIndex) => (prevIndex + 1) % profileData.length);
-    };
-  
-    const handleBackCard = () => {
-      setCurrentCardIndex((prevIndex) =>
-        prevIndex === 0 ? profileData.length - 1 : prevIndex - 1
-      );
-    };
+  const handleNextCard = () => {
+    setCurrentCardIndex((prevIndex) => (prevIndex + 1) % profileData.length);
+  };
 
-    return(
-        <Box className={styles.profile_cards}>
-        <video className={styles.teams_gradient}
-    
-    autoPlay
-    loop
-    muted
-  >
-    <source src="/bootcamp-hero-gradient.mp4" type="video/mp4" />
-  </video>
-  <Box className={styles.desktop}>
-    <Box className={styles.gridContainer}>
-    {profileData.map((m) => {
-      return (
-        <ProfileCard
-          name={m.name}
-          role={m.role}
-          hoveredText={m.description}
-          image={m.image}
-          hover={true}
-        />
-      );
-    })}
-    </Box>
-  </Box>
-  <Box className={styles.mobile}>
+  const handleBackCard = () => {
+    setCurrentCardIndex((prevIndex) =>
+      prevIndex === 0 ? profileData.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <Box className={styles.profile_cards}>
+      <video className={styles.teams_gradient} autoPlay loop muted>
+        <source src="/bootcamp-hero-gradient.mp4" type="video/mp4" />
+      </video>
+      <Box className={styles.desktop} style={{}}>
+        {/* <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          "@media (min-width: 640px)": {
+            display: "none",
+          },
+        }}
+      > */}
+        <Box className={styles.gridContainer}>
+          {profileData.map((m) => {
+            return (
+              <ProfileCard
+                name={m.name}
+                role={m.role}
+                hoveredText={m.description}
+                image={m.image}
+                hover={true}
+              />
+            );
+          })}
+        </Box>
+      </Box>
+      <Box className={styles.mobile}>
         <Box
           sx={{
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
-            padding: "40px"
+            padding: "40px",
           }}
         >
           <IconButton
@@ -77,7 +82,6 @@ export default function TeamProfile() {
           </IconButton>
         </Box>
       </Box>
-</Box>
-    );
-
+    </Box>
+  );
 }
