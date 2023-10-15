80932253252b94de7d0e25492cb965b1d9f4c604
emmyxth <emmyst@stanford.edu>
2023-10-15 16:12:14 -0700

    changed fonts for what youll do bootcamp

--- components/bootcamp/WhatYoullDoComponent.js
+++ components/bootcamp/WhatYoullDoComponent.js
@@ -1,29 +1,32 @@
+import { Typography } from "@mui/material";
+
 export default function WhatYoullDo({ header, text, img }) {
   return (
     <div className="flex flex-col items-center text-center">
       <img src={img} />
-      <h2
+      <h1
         style={{
           fontSize: "calc(12px + 1vmin)",
           fontWeight: "normal",
-          fontFamily: "Inter",
+          // fontFamily: "Inter",
           color: "black",
           maxWidth: "100%",
+          paddingTop: "2%",
         }}
       >
         {header}
-      </h2>
-      <p
-        style={{
+      </h1>
+      <Typography
+        sx={{
           fontSize: "calc(9px + 1vmin)",
-          fontWeight: "normal",
-          fontFamily: "Inter",
+          fontWeight: "300",
           color: "#667085",
           width: "50%",
+          paddingTop: "2%",
         }}
       >
         {text}
-      </p>
+      </Typography>
     </div>
   );
 }
